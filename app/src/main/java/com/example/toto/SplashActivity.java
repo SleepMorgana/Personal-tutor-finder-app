6d6fd7e9078a172aa306dec6cc71b0e726bc60b8
Oluwole Fadeyi <oluwole.fadeyi@jetstack.io>
2019-03-31 14:22:34 +0100

    Add Splash Screen
    Added splash screen as the launcher activity and set sign in activity as
    default.
    
    Related #6

---
+++ app/src/main/java/com/example/toto/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.toto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+public class SplashActivity extends Activity {
+    //Use splash screen to perform checks on device, like if the device is connected to
+    //the network or the user as already logged in
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //At the moment we aren't performing any checks just sleeping
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);  //Delay of 2 seconds
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "There were some issues loading the application, please try again",
+                            Toast.LENGTH_SHORT).show();
+                } finally {
+                    Intent intent = new Intent(getApplicationContext(),SignInSignUp.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }.start();
+    }
+}
